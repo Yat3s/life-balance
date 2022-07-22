0438ea1161d0aed88c3ea05089f899192cd91a9f
Chris Ye <hawkoyates@gmail.com>
2022-07-22 17:27:24 +0800

    Revert "finish glossaryfunction"
    This reverts commit fe5f6e177898c00020dfd47bcec6f049e5240965.

--- cloudfunctions/glossaryFunctions/index.js
+++
@@ -1,9 +0,0 @@
-const queryGlossary = require('./actions/queryGlossary');
-
-exports.main = async (event, context) => {
-  const query = event.data.query;
-  switch (event.action) {
-    case 'queryGlossary':
-      return await queryGlossary.main(query, context);
-  }
-}
