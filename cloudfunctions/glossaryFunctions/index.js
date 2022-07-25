9d8401069da3771bd89a55329561be464588b9a3
jianmaMS <t-jianma@microsoft.com>
2022-07-25 17:46:04 +0800

    delete some test code

--- cloudfunctions/glossaryFunctions/index.js
+++ cloudfunctions/glossaryFunctions/index.js
@@ -7,13 +7,3 @@ exports.main = async (event, context) => {
       return await queryGlossary.main(query, context);
   }
 }
-
-
-const eve = {
-  data: {
-    query: "om"
-  },
-  action: "queryGlossary"
-}
-const res = this.main(eve, "")
-res.then(e => console.log(e))
