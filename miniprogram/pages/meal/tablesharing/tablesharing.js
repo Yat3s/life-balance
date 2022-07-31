9bc2c1e41586d169c69866cea2b8e22997ea9cfa
Yajing Fang <yajfang@microsoft.com>
2022-07-31 21:39:31 +0800

    add some css and ui elements

--- miniprogram/pages/meal/tablesharing/tablesharing.js
+++ miniprogram/pages/meal/tablesharing/tablesharing.js
@@ -6,25 +6,38 @@ Page({
    * Page initial data
    */
   data: {
-    selected:false,
-    selecting: false,
-    table:"请选择",
-    tables:[
-      "A11","A12","A21","A22","A23","A24","A25","A26","A31","A32","A33","A34","A35","A41","A42","B11","B12","B13","B14","B15","B16","B21","B22","B23","B24","B25","B31","B32","B33","B34","B35","B41","B42","B43","B44","B45","C11","C12","C13","C14","C21","C22","C23","C31","C32","C33","C41","C42","C51","C52","C53","C54","D11","D12","D13","D14","D21","D22","D23","D31","D32","D33","D41","D42","D43","D51","D52","D53","D61"
-    ]
+    areaSelected: true,
+    tableSelected: true,
+    tableConfirmed: false,
+    selecting: true,
+    area:"A",
+    index:"11",
+    tableMap:{
+      A: [11, 12, 21, 22, 23, 24, 25, 26, 31, 32, 33, 34, 35, 41, 42],
+      B: [11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25, 31, 32, 33, 34, 35, 41, 42, 43, 44, 45],
+      C: [11, 12, 13, 14, 21, 22, 23, 31, 32, 33, 41, 42, 51, 52, 53, 54],
+      D: [11, 12, 13, 14, 21, 22, 23, 31, 32, 33, 41, 42, 43, 51, 52, 53, 61],
+      VIP: [1, 2]
+    }
   },
 
   bindShowMsg() {
     this.setData({
       selecting: !this.data.selecting,
     })
+
     // re-select
-    if (this.data.selected){
+    if (this.data.tableSelected){
       this.setData({
-        table: "请选择",
-        selected: false
+        area: "",
+        index: "",
+        areaSelected: false,
+        tableSelected: false,
+        tableConfirmed: false,
       })
     }
+    console.log(this.data.tableMap);
+    console.log(this.data.tableSelected);
   },
 
   /**
@@ -79,21 +92,47 @@ Page({
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage() {
-
+  onShareAppMessage(e) {
+    console.log("onShareAppMessage" + e)
     return {
       title: 'Canteen Table:' + this.data.table,
-      path: '/pages/meal/tabledetail/tabledetail?table='+this.data.table
+      path: '/pages/meal/tabledetail/tabledetail?table='+this.data.area+this.data.index
     }
   },
 
   onTableSelected(e) {
-    console.log(e)
+    console.log("onTableSelected")
     var name = e.currentTarget.dataset.name
+  
     this.setData({
-      table: name,
-      selected: true,
-      selecting: false
+      index:name,
+      selecting: true,
+      areaSelected: true,
+      tableSelected: true,
+      tableConfirmed: false,
+    })
+  },
+
+  onTableConfirmed(e) {
+    console.log("onTableConfirmed")
+    this.setData({
+      tableConfirmed:true,
+      selecting: false,
+      areaSelected: false,
+      tableSelected: false,
+      tableConfirmed: true
+    })
+  },
+
+  onAreaSelected(e) {
+    console.log("onAreaSelected")
+    this.setData({
+      area:e.currentTarget.dataset.name,
+      index:"",
+      selecting: true,
+      areaSelected: true,
+      tableSelected: false,
+      tableConfirmed:false,
     })
   }
 })
