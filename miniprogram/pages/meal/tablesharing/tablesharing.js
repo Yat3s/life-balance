1d7265b90f6b465c446ede973345f5ab4ebfb33e
yajfang <yajfang@microsoft.com>
2022-09-19 15:18:19 +0800

    add picture touch scale

--- miniprogram/pages/meal/tablesharing/tablesharing.js
+++ miniprogram/pages/meal/tablesharing/tablesharing.js
@@ -20,7 +20,70 @@ Page({
       C: [11, 12, 13, 14, 21, 22, 23, 31, 32, 33, 41, 42, 51, 52, 53, 54],
       D: [11, 12, 13, 14, 21, 22, 23, 31, 32, 33, 41, 42, 43, 51, 52, 53, 61],
       VIP: [1, 2]
+      scale    },
+    touch: {
+      distance: 0,
+      scale: 1,
+      baseWidth: null,
+      baseHeight: null,
+      scaleWidth: null,
+      scaleHeight: null
     }
+  },
+
+  touchstartCallback: function(e) {
+    // 单手指缩放开始，也不做任何处理
+    if(e.touches.length == 1) return
+    console.log('双手指触发开始')
+    // 注意touchstartCallback 真正代码的开始
+    // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug
+    // 当两根手指放上去的时候，就将distance 初始化。
+    let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+    this.setData({
+       'touch.distance': distance,
+    })
+  },
+
+  touchmoveCallback: function(e) {
+    let touch = this.data.touch
+    // 单手指缩放我们不做任何操作
+    if(e.touches.length == 1) return
+    console.log('双手指运动')
+    let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    // 新的 ditance
+    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+    let distanceDiff = distance - touch.distance;
+    let newScale = touch.scale + 0.005 * distanceDiff
+    // 为了防止缩放得太大，所以scale需要限制，同理最小值也是
+    if(newScale >= 3) {
+        newScale = 3
+    }
+    if(newScale <= 0.4) {
+        newScale = 0.4
+    }
+    let scaleWidth = newScale * touch.baseWidth
+    let scaleHeight = newScale * touch.baseHeight
+    // 赋值 新的 => 旧的
+    this.setData({
+       'touch.distance': distance,
+       'touch.scale': newScale,
+       'touch.scaleWidth': scaleWidth,
+       'touch.scaleHeight': scaleHeight,
+       'touch.diff': distanceDiff
+    })
+  },
+
+  bindload: function(e) {
+    // bindload 这个api是<image>组件的api类似<img>的onload属性
+    this.setData({
+        'touch.baseWidth': e.detail.width,
+        'touch.baseHeight': e.detail.height,
+        'touch.scaleWidth': e.detail.width,
+        'touch.scaleHeight': e.detail.height
+    })
   },
 
   bindShowMsg() {
