2d893ea264c0a9cb5c0d001bee394c721c1b391d
Yajing Fang <yajfang@microsoft.com>
2022-09-21 00:05:43 +0800

    coordinates

--- miniprogram/pages/meal/tablesharing/tablesharing.js
+++ miniprogram/pages/meal/tablesharing/tablesharing.js
@@ -21,32 +21,20 @@ Page({
       D: [11, 12, 13, 14, 21, 22, 23, 31, 32, 33, 41, 42, 43, 51, 52, 53, 61],
       VIP: [1, 2]
     },
+    tableMap1:{
+      A: {"11": [45, 68], "12": [45, 68]},
+      B: {"11": [45, 98], "12": [45, 68]},
+      C: {"11": [45, 68], "12": [75, 168]},
+      D: {"11": [55, 68], "12": [45, 68]},
+      VIP:{"1": [600, 350], "2": [800, 400]}
+    },
     touch: {
-      distance: 0,
       scale: 1,
       baseWidth: 814,
       baseHeight: 457,
       scaleWidth: 814,
       scaleHeight: 457
     }
-  },
-
-  touchstartCallback: function(e) {
-    // 单手指缩放开始，也不做任何处理
-    if(e.touches.length == 1) return
-    console.log('双手指触发开始' + e)
-    console.log(e)
-    // 注意touchstartCallback 真正代码的开始
-    // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug
-    // 当两根手指放上去的时候，就将distance 初始化。
-    let xMove = e.touches[1].x - e.touches[0].x;
-    let yMove = e.touches[1].y - e.touches[0].y;
-    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
-    this.setData({
-       'touch.distance': distance,
-    })
-
-    console.log("distance:"+this.data.touch.distance);
   },
 
   scaleUp: function(e) {
@@ -86,43 +74,6 @@ Page({
     console.log(this.data.touch)
   },
 
-  touchmoveCallback: function(e) {
-    let touch = this.data.touch
-    // 单手指缩放我们不做任何操作
-    if(e.touches.length == 1) return
-    console.log('双手指运动' + e)
-    console.log(e)
-    let xMove = e.touches[1].x - e.touches[0].x;
-    let yMove = e.touches[1].y - e.touches[0].y;
- 
-    // 新的 ditance
-    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
-    let distanceDiff = distance - touch.distance;
-    let newScale = touch.scale + 0.005 * distanceDiff
-    // 为了防止缩放得太大，所以scale需要限制，同理最小值也是
-    if(newScale >= 3) {
-        newScale = 3
-    }
-    if(newScale <= 0.4) {
-        newScale = 0.4
-    }
-
-    console.log("X: " + xMove + " Y: " + yMove + " distance: " + distance + " newScale: " + newScale);
-
-    let scaleWidth = newScale * touch.baseWidth
-    let scaleHeight = newScale * touch.baseHeight
-    // 赋值 新的 => 旧的
-    this.setData({
-       'touch.distance': distance,
-       'touch.scale': newScale,
-       'touch.scaleWidth': scaleWidth,
-       'touch.scaleHeight': scaleHeight,
-       'touch.diff': distanceDiff
-    })
-
-    console.log(this.data.touch);
-  },
-
   bindShowMsg() {
     this.setData({
       selecting: !this.data.selecting,
@@ -135,6 +86,7 @@ Page({
         index: "",
       })
     }
+
     if (this.data.selecting) {
       this.setData({
         area: "A",
@@ -143,7 +95,7 @@ Page({
       })
     }
 
-    console.log(this.data.tableMap);
+    console.log(this.data.tableMap)
   },
 
   /**
@@ -155,23 +107,6 @@ Page({
         userInfo
       })
     })
-
-    const drawAarrow = (context, fromx, fromy, tox, toy) => {
-        var headlen = 10; // length of head in pixels
-        var dx = tox - fromx;
-        var dy = toy - fromy;
-        var angle = Math.atan2(dy, dx);
-
-        context.beginPath()
-        context.moveTo(fromx, fromy);
-        context.lineTo(tox, toy);
-        context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
-        context.moveTo(tox, toy);
-        context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
-        context.strokeStyle = "red"
-        context.lineWidth = 3
-        context.stroke();
-    };
 
     // 通过 SelectorQuery 获取 Canvas 节点
     wx.createSelectorQuery()
@@ -198,8 +133,9 @@ Page({
         img.onload = (e) => {
             console.log(ctx)
             ctx.drawImage(img, 0, 0)
-            drawAarrow(ctx, 256, 0, 45, 68)
         }
+
+        //ctx.save()
 
         wx.getImageInfo({
             src: 'cloud://life-6go5gey72a61a773.6c69-life-6go5gey72a61a773-1259260883/app-assets/canteen-pics/Default.png',
@@ -270,6 +206,43 @@ Page({
 
   onTableConfirmed(e) {
     console.log("onTableConfirmed")
+
+    const drawAarrow = (context, fromx, fromy, tox, toy) => {
+      var headlen = 10; // length of head in pixels
+      var dx = tox - fromx;
+      var dy = toy - fromy;
+      var angle = Math.atan2(dy, dx);
+
+      context.beginPath()
+      context.moveTo(fromx, fromy);
+      context.lineTo(tox, toy);
+      context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+      context.moveTo(tox, toy);
+      context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+      context.strokeStyle = "red"
+      context.lineWidth = 3
+      context.stroke();
+      context.endPath()
+  };
+
+  // 通过 SelectorQuery 获取 Canvas 节点
+  wx.createSelectorQuery()
+    .select('#myCanvas')
+    .fields({
+      node: true,
+      size: true,
+    })
+    .exec((res) => {
+      console.log(res)
+
+      const canvas = res[0].node
+      const ctx = canvas.getContext('2d')
+      const obj = this.data.tableMap1[this.data.area]
+      console.log(obj)
+      const cords = obj[this.data.index]
+      console.log(cords)
+      drawAarrow(ctx, 256, 0, cords[0], cords[1])
+    })
 
     this.setData({
       tableConfirmed:true,
