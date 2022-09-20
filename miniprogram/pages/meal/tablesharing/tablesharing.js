29a8fd5efab54d4f6c1456a5de71c54f3779fec0
yajfang <yajfang@microsoft.com>
2022-09-20 20:13:25 +0800

    use canvas

--- miniprogram/pages/meal/tablesharing/tablesharing.js
+++ miniprogram/pages/meal/tablesharing/tablesharing.js
@@ -20,39 +20,44 @@ Page({
       C: [11, 12, 13, 14, 21, 22, 23, 31, 32, 33, 41, 42, 51, 52, 53, 54],
       D: [11, 12, 13, 14, 21, 22, 23, 31, 32, 33, 41, 42, 43, 51, 52, 53, 61],
       VIP: [1, 2]
-      scale    },
+    },
     touch: {
       distance: 0,
       scale: 1,
-      baseWidth: null,
-      baseHeight: null,
-      scaleWidth: null,
-      scaleHeight: null
+      baseWidth: 814,
+      baseHeight: 457,
+      scaleWidth: 814,
+      scaleHeight: 457
     }
   },
 
   touchstartCallback: function(e) {
     // 单手指缩放开始，也不做任何处理
     if(e.touches.length == 1) return
-    console.log('双手指触发开始')
+    console.log('双手指触发开始' + e)
+    console.log(e)
     // 注意touchstartCallback 真正代码的开始
     // 一开始我并没有这个回调函数，会出现缩小的时候有瞬间被放大过程的bug
     // 当两根手指放上去的时候，就将distance 初始化。
-    let xMove = e.touches[1].clientX - e.touches[0].clientX;
-    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    let xMove = e.touches[1].x - e.touches[0].x;
+    let yMove = e.touches[1].y - e.touches[0].y;
     let distance = Math.sqrt(xMove * xMove + yMove * yMove);
     this.setData({
        'touch.distance': distance,
     })
+
+    console.log("distance:"+this.data.touch.distance);
   },
 
   touchmoveCallback: function(e) {
     let touch = this.data.touch
     // 单手指缩放我们不做任何操作
     if(e.touches.length == 1) return
-    console.log('双手指运动')
-    let xMove = e.touches[1].clientX - e.touches[0].clientX;
-    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    console.log('双手指运动' + e)
+    console.log(e)
+    let xMove = e.touches[1].x - e.touches[0].x;
+    let yMove = e.touches[1].y - e.touches[0].y;
+ 
     // 新的 ditance
     let distance = Math.sqrt(xMove * xMove + yMove * yMove);
     let distanceDiff = distance - touch.distance;
@@ -64,6 +69,9 @@ Page({
     if(newScale <= 0.4) {
         newScale = 0.4
     }
+
+    console.log("X: " + xMove + " Y: " + yMove + " distance: " + distance + " newScale: " + newScale);
+
     let scaleWidth = newScale * touch.baseWidth
     let scaleHeight = newScale * touch.baseHeight
     // 赋值 新的 => 旧的
@@ -74,16 +82,8 @@ Page({
        'touch.scaleHeight': scaleHeight,
        'touch.diff': distanceDiff
     })
-  },
 
-  bindload: function(e) {
-    // bindload 这个api是<image>组件的api类似<img>的onload属性
-    this.setData({
-        'touch.baseWidth': e.detail.width,
-        'touch.baseHeight': e.detail.height,
-        'touch.scaleWidth': e.detail.width,
-        'touch.scaleHeight': e.detail.height
-    })
+    console.log(this.data.touch);
   },
 
   bindShowMsg() {
@@ -118,6 +118,62 @@ Page({
         userInfo
       })
     })
+
+    const drawAarrow = (context, fromx, fromy, tox, toy) => {
+        var headlen = 10; // length of head in pixels
+        var dx = tox - fromx;
+        var dy = toy - fromy;
+        var angle = Math.atan2(dy, dx);
+
+        context.beginPath()
+        context.moveTo(fromx, fromy);
+        context.lineTo(tox, toy);
+        context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+        context.moveTo(tox, toy);
+        context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+        context.strokeStyle = "red"
+        context.lineWidth = 3
+        context.stroke();
+    };
+
+    // 通过 SelectorQuery 获取 Canvas 节点
+    wx.createSelectorQuery()
+      .select('#myCanvas')
+      .fields({
+        node: true,
+        size: true,
+      })
+      .exec((res) => {
+        console.log(res)
+        const width = res[0].width
+        const height = res[0].height
+    
+        const canvas = res[0].node
+        const ctx = canvas.getContext('2d')
+    
+        const dpr = wx.getSystemInfoSync().pixelRatio
+        canvas.width = width * dpr
+        canvas.height = height * dpr
+        ctx.scale(dpr, dpr)
+    
+        const img = canvas.createImage()
+
+        img.onload = (e) => {
+            console.log(ctx)
+            ctx.drawImage(img, 0, 0)
+            drawAarrow(ctx, 256, 0, 45, 68)
+        }
+
+        wx.getImageInfo({
+            src: 'cloud://life-6go5gey72a61a773.6c69-life-6go5gey72a61a773-1259260883/app-assets/canteen-pics/Default.png',
+            success: function(res) {
+                console.log(res)
+                img.src = res.path
+            }
+          })
+
+        console.log(img.src)
+      })
   },
 
   /**
@@ -177,6 +233,7 @@ Page({
 
   onTableConfirmed(e) {
     console.log("onTableConfirmed")
+
     this.setData({
       tableConfirmed:true,
       selecting: false
