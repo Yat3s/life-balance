6f4160436fca7ffc59d2f3cff110217b35958c4c
Chris Ye <hawkoyates@gmail.com>
2024-04-07 14:50:09 +0800

    Support zhongment parking space (#142)

--- miniprogram/components/commute/commute.js
+++ miniprogram/components/commute/commute.js
@@ -5,7 +5,6 @@ import {
 import {
   fetchLastParkingFullTime,
   fetchParkingSpace,
-  fetchParkingSpacePrediction,
   recordParkingFull
 } from "../../repository/dashboardRepo"
 
@@ -14,21 +13,11 @@ Component({
   options: {
     addGlobalClass: true
   },
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
-    maxGroundSpaces: 0,
-    maxUndergroundSpaces: 512,
-    groundSpaceIndicatorWidth: "100%",
-    undergroundIndicatorWidth: "100%",
+    maxB25Spaces: 508,
+    maxZhongmengSpaces: 223,
+    zhongmengSpaceIndicatorWidth: "100%",
+    b25SpaceIndicatorWidth: "100%",
     loadingParkingSpace: true,
   },
 
@@ -39,13 +28,6 @@ Component({
     },
   },
 
-  pageLifetimes: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
   methods: {
     refresh() {
       this.setData({
@@ -63,27 +45,15 @@ Component({
     },
 
     fetchParkingSpacePredictionData() {
-      // fetchParkingSpacePrediction().then(res => {
-      //   if (!res) {
-      //     return;
-      //   }
-
-      //   const predictFullTime = (new Date(res)).hhmm();
-      //   console.log("fetchParkingSpacePredictionData", predictFullTime);
-      //   this.setData({
-      //     predictFullTime
-      //   })
-      // })
-
       fetchLastParkingFullTime().then(res => {
         let lastParkingFullTimeStr = ""
         let dayStrPrefix = (new Date()).getDay() == 1 ? '上周五' : '昨日'
         const showParkingFullTip = res != null
         if (res) {
           const lastParkingFullTime = (new Date(res)).hhmm();
-          lastParkingFullTimeStr = `${dayStrPrefix}停满时间：${lastParkingFullTime}`
+          lastParkingFullTimeStr = `${dayStrPrefix} B25 停满时间：${lastParkingFullTime}`
         } else {
-          lastParkingFullTimeStr = `${dayStrPrefix}车位充足`
+          lastParkingFullTimeStr = `${dayStrPrefix} B25 车位充足`
         }
         this.setData({
           showParkingFullTip,
@@ -94,18 +64,17 @@ Component({
 
     fetchParkingData() {
       const {
-        maxGroundSpaces,
-        maxUndergroundSpaces,
+        maxZhongmengSpaces,
+        maxB25Spaces,
       } = this.data;
       fetchParkingSpace().then(parkingSpace => {
         console.log("parkingSpace", parkingSpace);
-        const groundSpaceIndicatorWidth = (parkingSpace.ground / maxGroundSpaces) * 100 + "%"
-        const undergroundIndicatorWidth = (parkingSpace.underground / maxUndergroundSpaces) * 100 + "%"
-
+        const zhongmengSpaceIndicatorWidth = (parkingSpace.zhongmeng / maxZhongmengSpaces) * 100 + "%"
+        const b25SpaceIndicatorWidth = (parkingSpace.b25 / maxB25Spaces) * 100 + "%"
 
         const now = new Date();
         if (now.getHours() >= 9 && now.getHours() <= 13) {
-          const leftSpace = parkingSpace.ground + parkingSpace.underground;
+          const leftSpace = parkingSpace.b25;
           if (leftSpace <= 3) {
             recordParkingFull(Date.now());
           } else if (leftSpace <= 10) {
@@ -117,8 +86,8 @@ Component({
 
         this.setData({
           parkingSpace,
-          groundSpaceIndicatorWidth,
-          undergroundIndicatorWidth,
+          zhongmengSpaceIndicatorWidth,
+          b25SpaceIndicatorWidth,
           loadingParkingSpace: false,
         })
       });
