c31f579ec9cf263236a39d018f31be6dd8c1aafb
Chris Ye <hawkoyates@gmail.com>
2024-04-07 14:49:44 +0800

    Support zhongment parking space

--- cloudfunctions/dashboardFunctions/actions/fetchParkingSpace.js
+++ cloudfunctions/dashboardFunctions/actions/fetchParkingSpace.js
@@ -1,35 +1,34 @@
 const cloud = require('wx-server-sdk');
 const rp = require('request-promise');
-const AREA_GROUND = 15;
-const AREA_UNDERGROUND = 12;
+const AREA_ZHONGMENG = 2;
+const AREA_B25 = 12;
 
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
 });
-const db = cloud.database();
-
 exports.main = async (data, context) => {
-
   var options = {
     uri: `http://ms.suzhouparking.kingfaster.com.cn/ParkingSpaceApi/GetData`,
     json: true
   }
   const body = await rp.get(options);
   const { msparkingData } = body;
-  let ground = 0;
   let underground = 0;
+  let b25 = 0;
+  let zhongmeng = 0;
 
   for (const area of msparkingData) {
-    if (area.areaCode == AREA_UNDERGROUND) {
+    if (area.areaCode == AREA_B25) {
       underground = area.areaFreeSpaceNum;
+      b25 = area.areaFreeSpaceNum
     }
 
-    if (area.areaCode == AREA_GROUND) {
-      ground = area.areaFreeSpaceNum;
+    if (area.areaCode == AREA_ZHONGMENG) {
+      zhongmeng = area.areaFreeSpaceNum;
     }
   }
 
   return {
-    ground, underground
+    b25, zhongmeng, underground
   }
 }
