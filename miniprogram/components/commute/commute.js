65f2167ae3486a826dee1b8f9675b3201c9e9e1c
Levy Lv <lvw0522@gmail.com>
2024-11-05 22:30:35 +0800

    Fix module import path

--- miniprogram/components/commute/commute.js
+++ miniprogram/components/commute/commute.js
@@ -1,27 +1,23 @@
 import {
   navigateToBusInfo,
-  navigationToAppConfigWebView
-} from "../../pages/router";
+  navigationToAppConfigWebView,
+} from '../../pages/router';
 import {
-  fetchLastParkingFullTime,
   fetchLastWeekParkingFullTime,
   fetchParkingSpace,
-  recordParkingFull
-} from "../../repository/dashboardRepo"
-import {
-  getWeekdayIndexStr
-} from "../../util";
+  recordParkingFull,
+} from '../../repository/dashboardRepo';
+import { getWeekdayIndexStr } from '../../common/util';
 
-// components/commute/commute.js
 Component({
   options: {
-    addGlobalClass: true
+    addGlobalClass: true,
   },
   data: {
     maxB25Spaces: 508,
     maxZhongmengSpaces: 223,
-    zhongmengSpaceIndicatorWidth: "100%",
-    b25SpaceIndicatorWidth: "100%",
+    zhongmengSpaceIndicatorWidth: '100%',
+    b25SpaceIndicatorWidth: '100%',
     loadingParkingSpace: true,
   },
 
@@ -36,7 +32,7 @@ Component({
     refresh() {
       this.setData({
         loadingParkingSpace: true,
-      })
+      });
       this.fetchParkingData();
     },
 
@@ -45,40 +41,39 @@ Component({
     },
 
     toParkingTip() {
-      navigationToAppConfigWebView("parkingTip");
+      navigationToAppConfigWebView('parkingTip');
     },
 
     toShuttleTip() {
-      navigationToAppConfigWebView("shuttleTip");
+      navigationToAppConfigWebView('shuttleTip');
     },
 
     fetchParkingSpacePredictionData() {
-      fetchLastWeekParkingFullTime().then(res => {
-        let lastParkingFullTimeStr = ""
-        let dayStrPrefix = '上周' + getWeekdayIndexStr(new Date())
-        const showParkingFullTip = res != null
+      fetchLastWeekParkingFullTime().then((res) => {
+        let lastParkingFullTimeStr = '';
+        let dayStrPrefix = '上周' + getWeekdayIndexStr(new Date());
+        const showParkingFullTip = res != null;
         if (res) {
-          const lastParkingFullTime = (new Date(res)).hhmm();
-          lastParkingFullTimeStr = `${dayStrPrefix} B25 停满时间：${lastParkingFullTime}`
+          const lastParkingFullTime = new Date(res).hhmm();
+          lastParkingFullTimeStr = `${dayStrPrefix} B25 停满时间：${lastParkingFullTime}`;
         } else {
-          lastParkingFullTimeStr = `${dayStrPrefix} B25 车位充足`
+          lastParkingFullTimeStr = `${dayStrPrefix} B25 车位充足`;
         }
         this.setData({
           showParkingFullTip,
-          lastParkingFullTimeStr
-        })
-      })
+          lastParkingFullTimeStr,
+        });
+      });
     },
 
     fetchParkingData() {
-      const {
-        maxZhongmengSpaces,
-        maxB25Spaces,
-      } = this.data;
-      fetchParkingSpace().then(parkingSpace => {
-        console.log("parkingSpace", parkingSpace);
-        const zhongmengSpaceIndicatorWidth = (parkingSpace.zhongmeng / maxZhongmengSpaces) * 100 + "%"
-        const b25SpaceIndicatorWidth = (parkingSpace.b25 / maxB25Spaces) * 100 + "%"
+      const { maxZhongmengSpaces, maxB25Spaces } = this.data;
+      fetchParkingSpace().then((parkingSpace) => {
+        console.log('parkingSpace', parkingSpace);
+        const zhongmengSpaceIndicatorWidth =
+          (parkingSpace.zhongmeng / maxZhongmengSpaces) * 100 + '%';
+        const b25SpaceIndicatorWidth =
+          (parkingSpace.b25 / maxB25Spaces) * 100 + '%';
 
         const now = new Date();
         if (now.getHours() >= 9 && now.getHours() <= 13) {
@@ -97,8 +92,8 @@ Component({
           zhongmengSpaceIndicatorWidth,
           b25SpaceIndicatorWidth,
           loadingParkingSpace: false,
-        })
+        });
       });
-    }
-  }
-})
+    },
+  },
+});
