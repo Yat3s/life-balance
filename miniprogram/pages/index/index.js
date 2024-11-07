67485003a7c60b3bb56f4a282637249182f398c5
Levy Lv <lvw0522@gmail.com>
2024-11-07 15:06:22 +0800

    Add shopping tab

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,91 +1,104 @@
 const app = getApp();
-const {
-  getAppConfig
-} = require('../../repository/baseRepo');
-const userRepo = require('../../repository/userRepo');
+import { getAppConfig } from '../../repository/baseRepo';
+import {
+  fetchUserInfoOrSignup,
+  fetchUserInfo,
+} from '../../repository/userRepo';
 
 Page({
   data: {
-    showingModal: "",
-    currentTab: "board",
+    showingModal: '',
+    currentTab: 'board',
     navigationBarHeight: app.globalData.navigationBarHeight, // Safe area
     selectedGenderIndex: 0,
 
-    pages: [{
-        id: "board",
-        title: "Board",
-        icon: "../../images/ic_board.png",
-        iconActive: "../../images/ic_board_active.png",
+    pages: [
+      {
+        id: 'board',
+        title: 'Board',
+        icon: '../../images/ic_board.png',
+        iconActive: '../../images/ic_board_active.png',
+        isBeta: false,
       },
       {
-        id: "connection",
-        title: "Connection",
-        icon: "../../images/ic_connect.png",
-        iconActive: "../../images/ic_connect_active.png",
+        id: 'shopping',
+        title: 'Shopping',
+        icon: '../../images/ic_shopping.png',
+        iconActive: '../../images/ic_shopping_active.png',
+        isBeta: true,
       },
       {
-        id: "user",
-        title: "User",
-        icon: "../../images/ic_user.png",
-        iconActive: "../../images/ic_user_active.png",
-      }
-    ]
+        id: 'connection',
+        title: 'Connection',
+        icon: '../../images/ic_connect.png',
+        iconActive: '../../images/ic_connect_active.png',
+        isBeta: false,
+      },
+      {
+        id: 'user',
+        title: 'User',
+        icon: '../../images/ic_user.png',
+        iconActive: '../../images/ic_user_active.png',
+        isBeta: false,
+      },
+    ],
   },
 
-  onTabSelect: function (e) {
+  onTabSelect(e) {
     const currentTab = e.currentTarget.dataset.tabid;
     this.setData({
-      currentTab
+      currentTab,
     });
 
     const { isSignup } = this.data;
 
-    if ((currentTab === 'user' || currentTab === 'connection') && !app.globalData.userInfo && !isSignup) {
+    if (
+      (currentTab === 'user' || currentTab === 'connection') &&
+      !app.globalData.userInfo &&
+      !isSignup
+    ) {
       this.setData({
-        isSignup: true
+        isSignup: true,
       });
-      userRepo.fetchUserInfoOrSignup().then(res => {
-        this.setData({
-          isSignup: false
+      fetchUserInfoOrSignup()
+        .then((res) => {
+          this.setData({
+            isSignup: false,
+          });
+        })
+        .catch((error) => {
+          this.setData({
+            isSignup: false,
+          });
         });
-      }).catch(error => {
-        this.setData({
-          isSignup: false
-        });
-      });
     }
   },
 
-  onLoad: function (options) {
-    const {
-      windowWidth,
-      statusBarHeight
-    } = app.globalData;
+  onLoad(options) {
+    const { windowWidth, statusBarHeight } = app.globalData;
     this.setData({
       tabWidth: windowWidth / (this.data.pages.length + 1),
-      statusBarHeight
+      statusBarHeight,
     });
 
     // Ensure the authentication
-    userRepo.fetchUserInfo().then(userInfo => {
+    fetchUserInfo().then((userInfo) => {
       if (userInfo) {
         app.globalData.userInfo = userInfo;
       }
-    })
+    });
 
     // App Config
-    getAppConfig().then(config => {
-      const {
-        featureFlags,
-      } = config;
+    getAppConfig().then((config) => {
+      const { featureFlags } = config;
 
       const { pages } = this.data;
       const carpoolTabItem = {
-        id: "carpool",
-        title: "Carpool",
-        icon: "../../images/ic_carpool.png",
-        iconActive: "../../images/ic_carpool_active.png",
-      }
+        id: 'carpool',
+        title: 'Carpool',
+        icon: '../../images/ic_carpool.png',
+        iconActive: '../../images/ic_carpool_active.png',
+      };
 
       if (featureFlags.carpoolEnabled) {
         pages.splice(2, 0, carpoolTabItem);
@@ -93,44 +106,19 @@ Page({
 
       this.setData({
         featureFlags,
-        pages
-      })
-    })
+        pages,
+      });
+    });
   },
 
-  onReady: function () {
-
-  },
-
-  onShow: function () {
+  onShow() {
     if (app.globalData.pendingMessage) {
       wx.showToast({
         icon: 'none',
         duration: 3000,
         title: app.globalData.pendingMessage,
-      })
+      });
       app.globalData.pendingMessage = null;
     }
   },
-
-
-  onHide: function () {
-
-  },
-
-  onUnload: function () {
-
-  },
-
-  onPullDownRefresh: function () {
-
-  },
-
-  onReachBottom: function () {
-
-  },
-
-  onShareAppMessage: function () {
-
-  }
-})
+});
