878c018aa0d35fab48eece9992638d21a433991d
Levy Lv <lvw0522@gmail.com>
2024-11-14 15:00:02 +0800

    Refine code (#200)

---
+++ miniprogram/components/skeleton/index.js
@@ -0,0 +1,24 @@
+// components/skeleton/index.js
+Component({
+
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
