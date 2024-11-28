5484f93bc8986eb4c1582524ea3f57fe89094111
Levy Lv <lvw0522@gmail.com>
2024-11-28 13:30:38 +0800

    Rollback profile code (#288)

--- miniprogram/pages/user/profile/profile.js
+++ miniprogram/pages/user/profile/profile.js
@@ -1,179 +1,75 @@
-const { fetchAllPersonalCarpools } = require('../../repository/carpoolRepo');
-const activityRepo = require('../../repository/activityRepo');
-const userRepo = require('../../repository/userRepo');
-const router = require('../router');
-const app = getApp();
+const { formatDate } = require("../../../common/util");
+const { fetchUserActivities } = require("../../../repository/activityRepo");
+const { fetchUserWechatGroups } = require("../../../repository/dashboardRepo");
+const {
+  fetchUserProfile,
+  fetchCompany,
+} = require("../../../repository/userRepo");
+const { navigateToActivityDetail } = require("../../router");
 
-Component({
-  options: {
-    addGlobalClass: true,
-  },
-
-  properties: {},
-
+Page({
   data: {
-    windowWidth: app.globalData.windowWidth,
-    selectedTabId: 'all',
-    organizeCount: 0,
-    showEmpty: true,
-    tabs: [
-      {
-        id: 'all',
-        name: 'All activities',
-        count: 0,
-      },
-      {
-        id: 'organizer',
-        name: 'Posted activities',
-        count: 0,
-      },
-      {
-        id: 'like',
-        name: 'Favorite activities',
-        count: 0,
-      },
-      {
-        id: 'carpool',
-        name: 'All carpools',
-        count: 0,
-      },
-    ],
+    hasUserInfo: false,
   },
 
-  methods: {
-    onSettingCompanyClick() {
-      router.navigateToAuth();
-    },
-    onTabSelected(e) {
-      const selectedTabId = e.currentTarget.dataset.id;
-      const { activities, likedActivities, carpools } = this.data;
-
-      let showEmpty = false;
-      switch (selectedTabId) {
-        case 'all':
-          showEmpty = !activities || activities.length == 0;
-          break;
-
-        case 'organizer':
-          let count = 0;
-          activities.forEach((activity) => {
-            if (activity.type == 'organizer') {
-              count++;
-            }
-          });
-          showEmpty = count == 0;
-          break;
-
-        case 'like':
-          showEmpty = !likedActivities || likedActivities.length == 0;
-          break;
-
-        case 'carpool':
-          showEmpty = !carpools || carpools.length == 0;
-          break;
+  onLoad(options) {
+    const id = options.id;
+    fetchUserProfile(id).then((user) => {
+      console.log(user);
+      if (user.birthday) {
+        user.age =
+          new Date().getFullYear() - parseInt(user.birthday.slice(0, 4));
+        console.log(parseInt(user.birthday.slice(0, 4)));
       }
-
+      if (user.company) {
+        fetchCompany(user.company).then((company) => {
+          this.setData({ company });
+        });
+      }
+      // Check if user has any profile information
+      const hasUserInfo = !!(
+        user.height ||
+        user.weight ||
+        user.school ||
+        user.occupation ||
+        user.age
+      );
       this.setData({
-        selectedTabId,
-        showEmpty,
+        user,
+        hasUserInfo,
       });
-    },
+    });
 
-    fetchAllLikedActivities(ids) {
-      if (!ids) {
-        return;
-      }
-
-      activityRepo.fetchActivitiesByIds(ids).then((likedActivities) => {
-        this.setData({
-          likedActivities,
-        });
-
-        this.updateTabCount('like', likedActivities.length);
+    fetchUserActivities(id).then((activities) => {
+      console.log("fetchUserActivities", activities);
+      activities.forEach((activity) => {
+        activity.isOrganizer = activity.organizer._id === id;
+        activity.createDateStr = formatDate(activity._createTime);
       });
-    },
-
-    fetchAllPersonalActivities(openid) {
-      activityRepo.fetchAllPersonalActivities().then((activities) => {
-        let organizeCount = 0;
-        activities.forEach((activity) => {
-          if (activity.organizer._openid == openid) {
-            activity.type = 'organizer';
-            organizeCount++;
-          }
-        });
-
-        this.setData({
-          activities,
-          showEmpty: activities.length == 0,
-        });
-
-        this.updateTabCount('organizer', organizeCount);
-        this.updateTabCount('all', activities.length);
-      });
-    },
-
-    onUserInfoClick(e) {
-      router.navigate(router.Pages.UserInfo);
-    },
-
-    updateTabCount(tabId, count) {
-      const { tabs } = this.data;
-      for (const tab of tabs) {
-        if (tab.id == tabId) {
-          tab.count = count;
-          break;
-        }
-      }
-
       this.setData({
-        tabs,
+        activities,
       });
-    },
+    });
+
+    fetchUserWechatGroups(id).then((groups) => {
+      this.setData({
+        groups,
+      });
+    });
   },
 
-  pageLifetimes: {
-    show() {
-      // Update profile
-      userRepo.fetchUserInfo().then((userInfo) => {
-        if (!userInfo) {
-          return;
-        }
-
-        this.setData({
-          userInfo,
-        });
-      });
-    },
+  onPhotoClick(e) {
+    wx.previewImage({
+      urls,
+    });
   },
 
-  lifetimes: {
-    attached() {
-      userRepo.fetchUserInfo().then((userInfo) => {
-        if (!userInfo) {
-          return;
-        }
-
-        this.setData({
-          userInfo,
-        });
-
-        this.fetchAllPersonalActivities(userInfo._openid);
-        this.fetchAllLikedActivities(userInfo.likes);
-
-        fetchAllPersonalCarpools().then((carpools) => {
-          this.setData({
-            carpools,
-          });
-          this.updateTabCount('carpool', carpools.length);
-        });
-
-        if (userInfo.company) {
-          userRepo.fetchCompany(userInfo.company).then((company) => {
-            this.setData({ company });
-          });
-        }
-      });
-    },
+  onActivityItemClicked(e) {
+    const id = e.currentTarget.dataset.id;
+    navigateToActivityDetail(id);
   },
+
+  onShareAppMessage() {},
+
+  onShareTimeline() {},
 });
