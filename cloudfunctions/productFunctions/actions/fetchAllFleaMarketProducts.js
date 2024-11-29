947c4ffe03e692b124afc1f9f66d265e98df3b7c
Levy Lv <lvw0522@gmail.com>
2024-11-29 14:38:10 +0800

    Update product owner info

--- cloudfunctions/productFunctions/actions/fetchAllFleaMarketProducts.js
+++ cloudfunctions/productFunctions/actions/fetchAllFleaMarketProducts.js
@@ -1,11 +1,16 @@
-const cloud = require('wx-server-sdk');
-const { getAllData } = require('../lib/utils');
+const cloud = require("wx-server-sdk");
+const { getAllData } = require("../lib/utils");
 
-cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV });
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV,
+});
+
 const db = cloud.database();
+const _ = db.command;
 
-const COLLECTION_NAME_FLEA_MARKET_PRODUCTS = 'flea-market-products';
-const SALE_STATUS_ON = 'on';
+const COLLECTION_NAME_FLEA_MARKET_PRODUCTS = "flea-market-products";
+const COLLECTION_NAME_USERS = "users";
+const SALE_STATUS_ON = "on";
 
 exports.main = async (props, context) => {
   try {
@@ -15,21 +20,43 @@ exports.main = async (props, context) => {
         status: SALE_STATUS_ON,
       },
       orderBy: {
-        field: 'updatedAt',
-        type: 'desc',
+        field: "updatedAt",
+        type: "desc",
       },
     });
 
+    // Extract unique userIds
+    const userIds = [...new Set(products.map((product) => product.userId))];
+
+    // Batch fetch user information
+    const userList = await db
+      .collection(COLLECTION_NAME_USERS)
+      .where({
+        _openid: _.in(userIds),
+      })
+      .get()
+      .then((res) => res.data);
+
+    const userMap = userList.reduce((acc, user) => {
+      acc[user._openid] = user;
+      return acc;
+    }, {});
+
+    const productsWithUsers = products.map((product) => ({
+      ...product,
+      user: userMap[product.userId],
+    }));
+
     return {
       success: true,
-      data: products,
-      message: 'Data retrieved successfully',
+      data: productsWithUsers,
+      message: "Data retrieved successfully",
     };
   } catch (error) {
     return {
       success: false,
       error: error.message,
-      message: 'Failed to retrieve data',
+      message: "Failed to retrieve data",
     };
   }
 };
