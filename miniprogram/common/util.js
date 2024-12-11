39c6817933b4dda3d53a3fe9b6e7a55b7ae464d3
kezhang <91486896+Wotagei@users.noreply.github.com>
2024-12-11 15:12:56 +0800

    Update the trigger logic for the comment operation (#333)

--- miniprogram/common/util.js
+++ miniprogram/common/util.js
@@ -111,11 +111,13 @@ export const formatNumberWithCommas = (num) => {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
-export const formatDateWithDotSeparator = (timestamp) => {
+export const formatDateTimeWithDotSeparator = (timestamp) => {
   const date = new Date(timestamp);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
 
-  return `${year}.${month}.${day}`;
+  return `${year}.${month}.${day} ${hour}:${minute}`;
 };
