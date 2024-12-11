53ab7196e77903751209eeb24a603967c6d1f75b
Levy Lv <lvw0522@gmail.com>
2024-12-11 21:49:59 +0800

    Update lottery ui

--- miniprogram/components/lottery-card/index.js
+++ miniprogram/components/lottery-card/index.js
@@ -1,7 +1,7 @@
 import { navigateToLottery } from "../../pages/router";
 import { fetchLatestLottery as _fetchLatestLottery } from "../../repository/lotteryRepo";
 
-const MAX_DISPLAY = 6;
+const MAX_DISPLAY = 3;
 
 Component({
   options: {
@@ -39,6 +39,7 @@ Component({
       this.setData({
         displayParticipants: participants.slice(0, MAX_DISPLAY),
         hasMoreParticipants: totalParticipants > MAX_DISPLAY,
+        totalParticipants,
         remainingCount:
           totalParticipants > MAX_DISPLAY ? totalParticipants - MAX_DISPLAY : 0,
       });
