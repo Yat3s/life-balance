2d154e2b7a4cc57ca7dcf3eb8cf39a822b05dd2d
kezhang <91486896+Wotagei@users.noreply.github.com>
2024-12-16 22:21:04 +0800

    Update the display method for remaining parking spaces. (#377)

--- miniprogram/components/commuteV2/commuteV2.js
+++ miniprogram/components/commuteV2/commuteV2.js
@@ -12,6 +12,7 @@ const EMPTY_COLOR = "#3679EE";
 const BUSY_COLOR = "#FF593B";
 const SPIN_DURATION = 0.3;
 const COLOR_CHANGE_DURATION = 0.6;
+const SPACES_PER_INDICATOR = 45;
 
 Component({
   options: {
@@ -29,25 +30,51 @@ Component({
         usedPercent: 0,
         initialColor: EMPTY_COLOR,
         finalColor: EMPTY_COLOR,
+        remainingIndicatorCount: 0,
+        indicators: [],
       },
       zhongmeng: {
         remaining: 223,
         usedPercent: 0,
         initialColor: EMPTY_COLOR,
         finalColor: EMPTY_COLOR,
+        remainingIndicatorCount: 0,
+        indicators: [],
       },
     },
   },
 
   lifetimes: {
     attached() {
+      this.initIndicators();
       this.fetchParkingData();
       this.fetchParkingSpacePredictionData();
     },
   },
 
   methods: {
+    initIndicators() {
+      const { parkingConfig } = this.data;
+      const b25IndicatorCount = Math.round(
+        parkingConfig.b25.maxSpaces / SPACES_PER_INDICATOR
+      );
+      const b25Indicators = Array.from({ length: b25IndicatorCount }, () => ({
+        active: false,
+      }));
+      const zhongmengIndicatorCount = Math.round(
+        parkingConfig.zhongmeng.maxSpaces / SPACES_PER_INDICATOR
+      );
+      const zhongmengIndicators = Array.from(
+        { length: zhongmengIndicatorCount },
+        () => ({ active: false })
+      );
+      this.setData({
+        "parkingSpace.b25.indicators": b25Indicators,
+        "parkingSpace.zhongmeng.indicators": zhongmengIndicators,
+      });
+    },
     refresh() {
+      this.initIndicators();
       this.setData({
         loadingParkingSpace: true,
       });
@@ -83,16 +110,27 @@ Component({
     },
 
     fetchParkingData() {
-      fetchParkingSpace().then((parkingSpace) => {
-        console.log("parkingSpace", parkingSpace);
+      const { parkingSpace } = this.data;
+      fetchParkingSpace().then((parkingSpaceData) => {
+        console.log("parkingSpace", parkingSpaceData);
 
         const updatedParkingData = {};
         Object.keys(this.data.parkingConfig).forEach((key) => {
           const maxSpaces = this.data.parkingConfig[key].maxSpaces;
-          const remaining = parkingSpace[key];
+          const remaining = parkingSpaceData[key];
           const used = maxSpaces - remaining;
           const usedPercent = Math.round((used / maxSpaces) * 100);
 
+          const remainingIndicatorCount = Math.floor(
+            remaining / SPACES_PER_INDICATOR
+          );
+          const indicators =
+            key === "b25"
+              ? parkingSpace.b25.indicators
+              : parkingSpace.zhongmeng.indicators;
+          indicators.forEach((indicator, index) => {
+            indicator.active = index < remainingIndicatorCount;
+          });
           updatedParkingData[key] = {
             remaining,
             used,
@@ -101,6 +139,8 @@ Component({
             finalColor: usedPercent >= 90 ? BUSY_COLOR : EMPTY_COLOR,
             progressDuration: SPIN_DURATION,
             colorChangeDuration: COLOR_CHANGE_DURATION,
+            remainingIndicatorCount,
+            indicators,
           };
         });
 
