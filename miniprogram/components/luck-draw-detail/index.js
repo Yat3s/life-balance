627b19457a057deaf441e6b83fe8311126eac6ed
Levy Lv <lvw0522@gmail.com>
2024-12-23 14:46:15 +0800

    Clean code

--- miniprogram/components/luck-draw-detail/index.js
+++ miniprogram/components/luck-draw-detail/index.js
@@ -1,4 +1,4 @@
-const LUCK_DRAW_AD_TEMP_ID = "adunit-dce02371b9b9348d";
+const LUCK_DRAW_CUSTOM_AD_ID = "adunit-dce02371b9b9348d";
 
 Component({
   properties: {
@@ -14,27 +14,12 @@ Component({
 
   data: {
     hasParticipated: false,
-    customAd: null,
-    adId: LUCK_DRAW_AD_TEMP_ID,
+    adId: LUCK_DRAW_CUSTOM_AD_ID,
   },
 
   lifetimes: {
     attached() {
       this.checkParticipationStatus();
-    },
-
-    detached() {
-      if (this.data.customAd) {
-        this.data.customAd.destroy();
-      }
-    },
-  },
-
-  observers: {
-    "luckDraw, userInfo": function (luckDraw, userInfo) {
-      if (luckDraw && userInfo) {
-        this.checkParticipationStatus();
-      }
     },
   },
 
@@ -48,43 +33,6 @@ Component({
       );
 
       this.setData({ hasParticipated });
-    },
-
-    setupCustomAd() {
-      if (!wx.createRewardedVideoAd) return;
-
-      const customAd = wx.createRewardedVideoAd({
-        adUnitId: LUCK_DRAW_AD_TEMP_ID,
-      });
-
-      customAd.onLoad(() => {
-        console.log("Custom ad loaded successfully");
-      });
-
-      customAd.onError((err) => {
-        console.error("Failed to load custom video ad", err);
-      });
-
-      customAd.onClose(() => {
-        console.log("Custom ad closed");
-      });
-
-      this.setData({ customAd });
-    },
-
-    showPatchAd() {
-      if (!this.data.customAd) {
-        this.setupCustomAd();
-      }
-
-      this.data.customAd?.show().catch(() => {
-        this.data.customAd
-          ?.load()
-          .then(() => this.data.customAd.show())
-          .catch((err) => {
-            console.error("Failed to display custom ad", err);
-          });
-      });
     },
 
     previewImage() {
